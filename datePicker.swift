5163b4161f52d06a8143b2378469dcae03f83294
Nadia Ahmadian <nadia.ahmadian96@yahoo.com>
2023-02-12 20:57:54 +0330

    Create datePicker.swift

---
+++ datePicker.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+struct MainView: View {
+    @State var selectedDate = Date()
+    var dateClosedRange: ClosedRange<Date> {
+        let min = Calendar.current.date(byAdding: .day, value: -1, to: Date())!
+        let max = Calendar.current.date(byAdding: .day, value: 1, to: Date())!
+        return min...max
+    }
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    DatePicker(
+                        selection: $selectedDate,
+                        in: dateClosedRange,
+                        displayedComponents: .date,
+                        label: { Text("Due Date") }
+                    )
+                }
+            }
+        }
+    }
+}
+
